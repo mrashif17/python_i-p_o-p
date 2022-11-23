890ed2c5e4bc2e942c859fa7c64f6b3c209674c7
mrashif17 <114917085+mrashif17@users.noreply.github.com>
2022-11-23 21:53:04 +0530

    Create Python_str.py

---
+++ Python_str.py
@@ -0,0 +1,40 @@
+str = "iNeuron"
+ print(str)
+
+str2 = "Shashank's"
+print(str2)
+
+str3 = 'he is a "good" boy'
+print(str3)
+print(len(str3))
+
+str4 = """Shashank
+is  not 
+  going 
+  to attend 
+  the 
+  seminar"""
+print(str4)
+
+str5 = "Ashif"
+str6 = "khan"
+print(str5 + " " + str6)
+
+str5 = "Ashif"
+str6 = "khan"
+print(str5 == str6)
+
+    print(char)
+
+print(str5[0])
+print(str5[1])
+print(str5[2])
+print(str5[3])
+print(str5[4])
+
+str5 = "Ashif"
+str6 = "khan"
+str5[2] = "i"
+print(str5)
+
+print(str5.uppe())
